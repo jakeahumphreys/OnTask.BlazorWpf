c9fd0b5a26e3ada46b5ccfcc3c3008079d37376b
jakeahumphreys <jakehumphreys@outlook.com>
2022-10-20 19:44:24 +0100

    Added a repository class to handle EF operations for Collections

---
+++ OnTask.BlazorWpf/Data/Collections/CollectionRepository.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OnTask.BlazorWpf.Data.Collections;
+
+public class CollectionRepository
+{
+    private readonly AppDbContext _context;
+
+    public CollectionRepository()
+    {
+        _context = new AppDbContext();
+    }
+
+    public List<Collection> GetAll()
+    {
+        return _context.Collections.ToList();
+    }
+
+    public Collection GetById(Guid id)
+    {
+        return _context.Collections.SingleOrDefault(x => x.Id == id);
+    }
+
+    public void Create(Collection collection)
+    {
+        _context.Collections.Add(collection);
+        _context.SaveChangesAsync();
+    }
+
+    public void Update(Collection collection)
+    {
+        _context.Collections.Update(collection);
+        _context.SaveChanges();
+    }
+
+    public void Delete(Collection collection)
+    {
+        _context.Collections.Remove(collection);
+        _context.SaveChanges();
+    }
+}
