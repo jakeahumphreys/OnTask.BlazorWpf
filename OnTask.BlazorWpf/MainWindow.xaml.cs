247ca01f675a32146c7a88d22faaa898a798cde6
jakeahumphreys <jakehumphreys@outlook.com>
2022-10-20 18:11:28 +0100

    Initial commit

---
+++ OnTask.BlazorWpf/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
+
+
+namespace OnTask.BlazorWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            serviceCollection.AddMudServices();
+
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+
+            InitializeComponent();
+        }
+    }
+}
