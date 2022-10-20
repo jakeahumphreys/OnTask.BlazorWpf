5f16bf00988c67444b6afc455ab8c8a6f2c078a8
jakeahumphreys <jakehumphreys@outlook.com>
2022-10-20 20:06:08 +0100

    Display some basic info about the collection

--- OnTask.BlazorWpf/Pages/Index.razor.cs
+++ OnTask.BlazorWpf/Pages/Index.razor.cs
@@ -1,9 +1,23 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Components;
+using OnTask.BlazorWpf.Data.Collections;
+using OnTask.BlazorWpf.Services;
+
 namespace OnTask.BlazorWpf.Pages;
 
 public partial class Index
 {
+    [Inject] private CollectionService CollectionService { get; set; }
+    
+    private List<Collection> Collections { get; set; }
+    
     protected override void OnInitialized()
     {
-        
+        Collections = CollectionService
+            .GetAllCollections()
+            .Where(x => x.IsArchived == false)
+            .OrderByDescending(x => x.IsFocused)
+            .ToList();
     }
 }
