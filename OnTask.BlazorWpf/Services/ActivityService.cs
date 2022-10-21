3f4a380d1b2d928aba90f91ff4914ea96232cd94
jakeahumphreys <jakehumphreys@outlook.com>
2022-10-21 15:28:03 +0100

    Added activity service

---
+++ OnTask.BlazorWpf/Services/ActivityService.cs
@@ -0,0 +1,19 @@
+using System;
+using OnTask.BlazorWpf.Data.Activities;
+
+namespace OnTask.BlazorWpf.Services;
+
+public sealed class ActivityService
+{
+    private readonly ActivityRepository _repository;
+
+    public ActivityService(ActivityRepository activityRepository)
+    {
+        _repository = activityRepository;
+    }
+
+    public Activity GetActivityById(Guid id)
+    {
+        return _repository.GetById(id);
+    }
+}
