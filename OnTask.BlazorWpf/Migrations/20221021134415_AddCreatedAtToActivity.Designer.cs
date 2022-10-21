e0364108fa6cbbaf20b4bda0dc2533ccecd1e259
jakeahumphreys <jakehumphreys@outlook.com>
2022-10-21 14:44:53 +0100

    Migrating acitivites

--- OnTask.BlazorWpf/Migrations/20221021134415_AddCreatedAtToActivity.Designer.cs
+++ OnTask.BlazorWpf/Migrations/20221021134415_AddCreatedAtToActivity.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OnTask.BlazorWpf.Data;
 
@@ -10,9 +11,11 @@ using OnTask.BlazorWpf.Data;
 namespace OnTask.BlazorWpf.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221021134415_AddCreatedAtToActivity")]
+    partial class AddCreatedAtToActivity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.0-rc.2.22472.11");
