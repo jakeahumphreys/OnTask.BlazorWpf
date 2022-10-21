25b2063c88eee6c64c45988c46a8d025c2d4e6d2
jakeahumphreys <jakehumphreys@outlook.com>
2022-10-21 13:33:04 +0100

    Migration to include activities on collections

---
+++ OnTask.BlazorWpf/Migrations/20221021123223_AddActivitiesToCollections.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnTask.BlazorWpf.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddActivitiesToCollections : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CollectionId",
+                table: "Activities",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Activities_CollectionId",
+                table: "Activities",
+                column: "CollectionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Activities_Collections_CollectionId",
+                table: "Activities",
+                column: "CollectionId",
+                principalTable: "Collections",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Activities_Collections_CollectionId",
+                table: "Activities");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Activities_CollectionId",
+                table: "Activities");
+
+            migrationBuilder.DropColumn(
+                name: "CollectionId",
+                table: "Activities");
+        }
+    }
+}
